d726b4e9911721710577170a82cc13f93382769e
colin <colinarscotthhk@gmail.com>
2021-10-30 07:19:07 -0400

    Added some more features to make tik tok less scuffed
    Its still pretty scuffed though. The way the Parser works is that it will delete any Url you post which is not the appropriate behavior. Need to Fix that real quick

--- src/main/java/com/crashing_waves/discord_bot/DiscordBotApplication.java
+++ src/main/java/com/crashing_waves/discord_bot/DiscordBotApplication.java
@@ -2,6 +2,7 @@ package com.crashing_waves.discord_bot;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageBuilder;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
-import java.io.File;
-import java.io.IOException;
+
+import java.util.concurrent.CompletableFuture;
 
 @SpringBootApplication
 public class DiscordBotApplication {
@@ -29,26 +30,16 @@ public class DiscordBotApplication {
     public DiscordApi discordApi() {
         String token = env.getProperty("TOKEN");
         DiscordApi api = new DiscordApiBuilder().setToken(token).setAllNonPrivilegedIntents().login().join();
-
+        LinkParser parser = new LinkParser();
         api.addMessageCreateListener(event -> {
-            if (event.getMessageContent().equals("ping")){
-                String url = "video.mp4";
-                String command = "python C:\\Users\\colin\\Documents\\Python_Tik_tok\\main.py";
-                try{
-                    Process p = Runtime.getRuntime().exec(command);
-                    p.waitFor();
-                } catch (IOException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-                new MessageBuilder().addAttachment(new File(url)).send(event.getChannel());
-                try{
-                    Process p = Runtime.getRuntime().exec("rm video.mp4");
-                    p.waitFor();
-                } catch (IOException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-                //EmbedBuilder embed = new EmbedBuilder().setAuthor(event.getMessageAuthor()).setFooter("feet");
-                //event.getChannel().sendMessage(embed);
+            if (parser.isWebURL(event.getMessageContent())){
+                Message test = new MessageBuilder()
+                        .setContent("Working...")
+                        .send(event.getChannel())
+                        .join();
+                parser.parseLink(event.getMessageContent()).send(event.getChannel());
+                event.deleteMessage();
+                test.delete();
             }
         });
 
---
+++ src/main/java/com/crashing_waves/discord_bot/LinkParser.java
@@ -0,0 +1,60 @@
+package com.crashing_waves.discord_bot;
+
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.regex.*;
+
+
+public class LinkParser {
+
+
+    private File tikTokParse(String url){
+        //run the python command to get the url
+        String command = "python ./src/main/python/TTAPI.py";
+        try{
+            Process p = Runtime.getRuntime().exec(command +" \"" +url + "\"");
+            p.waitFor();
+            return new File("Media/video.mp4");
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return new File("src/main/java/rescources/error.png");
+        }
+    }
+
+    //This method's job is to decide which URL parser will recieve and parse the URL
+    public MessageBuilder parseLink(String url){
+         String Tik_tok = ("https://[a-z]*.tiktok.com[^\s]*");
+
+         if (Pattern.matches(Tik_tok,url)){
+             MessageBuilder message = new MessageBuilder().addAttachment(tikTokParse(url));
+             try{
+                 File file = new File("./Media/Attributes.json");
+                 InputStream is = new FileInputStream(file);
+                 String jsonTxt = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+                 JSONObject json = new JSONObject(jsonTxt);
+                 String Title = json.getString("Title");
+                 String Author = json.getString("Author");
+                 String Pfp = json.getString("pfp");
+                 EmbedBuilder embed = new EmbedBuilder()
+                         .setAuthor(Author)
+                         .setDescription(Title)
+                         .setImage(Pfp);
+                 return message.addEmbed(embed);
+             } catch(JSONException | IOException e ){
+                 e.printStackTrace();
+             }
+             return message;
+         }
+         return new MessageBuilder().setContent("Oops, Im a bad bot, I can't Use that link yet");
+
+    }
+
+    public boolean isWebURL(String url){
+        return Pattern.matches("https://[^\s]*",url);
+    }
+}
