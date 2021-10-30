bf7f1fe8187c4e292964f26363adab3380e25753
colin <colinarscotthhk@gmail.com>
2021-10-30 03:30:47 -0400

    This Commit is incredibly scuffed. I was able to successfully download a video via tik tok share url. This is done in a very scuffed way. I used this python tik tok API to download the video to the system via a hardcoded URL. The discord bot runs the python script then sends the file. Depending on how long the video is this can take a while.

--- src/main/java/com/crashing_waves/discord_bot/DiscordBotApplication.java
+++ src/main/java/com/crashing_waves/discord_bot/DiscordBotApplication.java
@@ -2,12 +2,16 @@ package com.crashing_waves.discord_bot;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 public class DiscordBotApplication {
@@ -28,7 +32,23 @@ public class DiscordBotApplication {
 
         api.addMessageCreateListener(event -> {
             if (event.getMessageContent().equals("ping")){
-                event.getChannel().sendMessage("pong!");
+                String url = "video.mp4";
+                String command = "python C:\\Users\\colin\\Documents\\Python_Tik_tok\\main.py";
+                try{
+                    Process p = Runtime.getRuntime().exec(command);
+                    p.waitFor();
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+                new MessageBuilder().addAttachment(new File(url)).send(event.getChannel());
+                try{
+                    Process p = Runtime.getRuntime().exec("rm video.mp4");
+                    p.waitFor();
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //EmbedBuilder embed = new EmbedBuilder().setAuthor(event.getMessageAuthor()).setFooter("feet");
+                //event.getChannel().sendMessage(embed);
             }
         });
 
